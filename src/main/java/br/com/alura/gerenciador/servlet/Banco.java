b4d5f1548bd4eec8558e030815e13d89fdd65a25
r-afael <roliveira1111@gmail.com>
2021-12-25 18:39:44 -0300

    Adicionando novas classes e criando função de listagem de empresas

---
+++ src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -0,0 +1,24 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Banco {
+
+	private static List<Empresa> lista = new ArrayList<>();
+	
+	static {
+		Empresa empresa = new Empresa("Alura");
+		lista.add(empresa);
+		Empresa empresa2 = new Empresa("Caelum");
+		lista.add(empresa2);
+	}
+	
+	public void adiciona(Empresa empresa) {
+		lista.add(empresa);
+	}
+
+	public List<Empresa> getEmpresas(){
+		return Banco.lista;
+	}
+}
--- src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -16,11 +16,15 @@ import javax.servlet.http.HttpServletResponse;
 public class NovaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response)
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		System.out.println("Cadastrando nova empresa");
 		String nomeEmpresa = request.getParameter("nome");
+		Empresa empresa = new Empresa(nomeEmpresa);
+		
+		Banco banco = new Banco();
+		banco.adiciona(empresa);
 		
 		PrintWriter out = response.getWriter();
 		out.println("<html><body>Empresa " + nomeEmpresa +  " cadastrada com sucesso!</body></html>");
