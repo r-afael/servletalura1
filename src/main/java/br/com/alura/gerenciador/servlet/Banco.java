e7769c3add74bcf38c75a3f8f707a22a9a0f0ac2
r-afael <roliveira1111@gmail.com>
2021-12-28 02:53:33 -0300

    Adicionando funções de editar e remover empresa

--- src/main/java/br/com/alura/gerenciador/servlet/Banco.java
+++ src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -1,24 +1,57 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
 
 	private static List<Empresa> lista = new ArrayList<>();
+	private static Integer chaveSequencial = 1;
 	
 	static {
 		Empresa empresa = new Empresa("Alura");
+		empresa.setId(Banco.chaveSequencial++);
 		lista.add(empresa);
 		Empresa empresa2 = new Empresa("Caelum");
+		empresa2.setId(Banco.chaveSequencial++);
 		lista.add(empresa2);
 	}
 	
 	public void adiciona(Empresa empresa) {
+		empresa.setId(Banco.chaveSequencial++);
 		lista.add(empresa);
 	}
 
 	public List<Empresa> getEmpresas(){
 		return Banco.lista;
+	}
+
+	public void removeEmpresa(Integer id) {
+		
+		Iterator<Empresa> it = lista.iterator();
+		
+		while(it.hasNext()) {
+			Empresa emp = it.next();
+			if(emp.getId() == id) {
+				it.remove();
+			}
+		}
+		
+//		for (Empresa empresa : lista) {
+//			if(empresa.getId() == id) {
+//				lista.remove(empresa);
+//			}
+//		}
+		
+	}
+
+	public Empresa buscaEmpresaPelaId(Integer id) {
+		for (Empresa empresa : lista) {
+			if(empresa.getId() == id) {
+				return empresa;
+			}
+		}
+		return null;
 	}
 }
